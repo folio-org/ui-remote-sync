ea236b6d68afb8aa2b7f47899181340b951407f1
Ian Ibbotson <ianibbo@gmail.com>
2021-05-26 18:47:29 +0100

    Working on posting definitions URL

--- src/settings/DefinitionsPage.js
+++ src/settings/DefinitionsPage.js
@@ -7,10 +7,16 @@ import {
   List,
   Pane,
 } from '@folio/stripes/components';
-
 import { FormattedMessage } from 'react-intl';
+import { useOkapiKy } from '@folio/stripes/core';
+
 
 export default function DefinitionsPage({}) {
+
+  const ky = useOkapiKy();
+
+  // https://github.com/sindresorhus/ky
+  // const json = await ky.post('https://example.com', {json: {foo: true}}).json();
 
   const getLastMenu = () => {
     return (
@@ -26,8 +32,17 @@ export default function DefinitionsPage({}) {
   }
 
   const handleSubmit = (event) => {
-    console.log("submit");
     event.preventDefault()
+    console.log("submit2");
+    let config_call = async () => {
+      console.log("Post....");
+      const json = await ky.post('remote-sync/settings/configureFromRegister', 
+                                 {json: {url: 'nothing'}}).json();
+                                 // {json: {url: 'https://raw.githubusercontent.com/folio-org/mod-remote-sync/master/testdata/laser_registry.json'}}).json();
+      return json
+    }
+
+    config_call().then(console.log)
   }
 
   const formStyle = {
