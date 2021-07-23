5f952dcb6326e0110d292db9f069026a578c43cf
Ian Ibbotson <ianibbo@gmail.com>
2021-07-23 09:32:15 +0100

    start to flesh out transformation process record

--- src/components/Resources/TransformationProcessRecord.js
+++ src/components/Resources/TransformationProcessRecord.js
@@ -12,7 +12,26 @@ export default function TransformationProcessRecord({resource} : props) {
 
   return (
     <Pane>
-      TPR
+      {JSON.stringify(resource)}
+      <table>
+        <tbody>
+          <tr><td>processControlStatus</td><td>{resource.processControlStatus}</td></tr>
+          <tr><td>transformationStatus</td><td>{resource.transformationStatus}</td></tr>
+          <tr><td>sourceRecordId</td><td>{resource.sourceRecordId}</td></tr>
+          <tr><td>Status Report</td><td>
+            <table>
+              <tbody>
+                { JSON.parse(resource.statusReport).map( (sr) => (
+                    <tr>
+                      <td>{sr.ts}</td>
+                      <td>{sr.msg}</td>
+                    </tr>
+                  ) ) }
+              </tbody>
+            </table>
+          </td></tr>
+        </tbody>
+      </table>
     </Pane>
   );
 }
